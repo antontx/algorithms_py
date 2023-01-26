7c6d024f6e3593b5eee9d9df1f832a0ed14c2797
antontx <tarisjan@gmail.com>
2023-01-26 18:18:43 +0100

    added the actual swapping to rotate the matrix, cleaned up code

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -1,41 +1,29 @@
 from typing import List
+import numpy as np
 
 
 def main():
-    inp = [[1,2,3],[4,5,6],[7,8,9]]
-    out = [[7,4,1],[8,5,2],[9,6,3]]
+    example_matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    print(np.matrix(example_matrix))
 
-    inp2 = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-
-    rotate(inp)
-    print("---------")
-    rotate(inp2)
-
-
-
+    rotate(example_matrix)
+    print(np.matrix(example_matrix))
 
 
 def rotate(matrix: List[List[int]]) -> None:
-    size = len(matrix)**2
-    ring_width = len(matrix)
-    ring = 0
-    x,y = 0,0
+    ring_width = len(matrix)-1
 
     for ring in range(2):
-        print(f"---{ring=} {ring_width=}")
-
-
-        for j in range(0,ring_width-1):
+        for j in range(0,ring_width):
             x,y = ring+j,ring
+            store = matrix[y][x]
+
             for i in range(4):
-                nX, nY = ring_width-1-y+(ring*2),x
-                print(matrix[y][x])
+                nX, nY = ring_width-y+(ring*2),x
+                store,matrix[nY][nX] = matrix[nY][nX],store
                 x,y = nX,nY
 
         ring_width -= 2
-
-
-
 
 
 if __name__ == '__main__':
