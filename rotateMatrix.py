37fe94128d82652b3c6a2cfbbd4b7b13e4e4d79d
antontx <tarisjan@gmail.com>
2023-01-26 18:28:02 +0100

    now supports matrices of all sizes

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def main():
-    example_matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    example_matrix = [[2,29,20,26,16,28],[12,27,9,25,13,21],[32,33,32,2,28,14],[13,14,32,27,22,26],[33,1,20,7,21,7],[4,24,1,6,32,34]]
     print(np.matrix(example_matrix))
 
     rotate(example_matrix)
@@ -12,8 +12,10 @@ def main():
 
 def rotate(matrix: List[List[int]]) -> None:
     ring_width = len(matrix)-1
+    ring_count = int (len(matrix) / 2) + len(matrix) % 2
 
-    for ring in range(2):
+    for ring in range(ring_count):
+        print(f"{ring=} {ring_width=}")
         for j in range(0,ring_width):
             x,y = ring+j,ring
             store = matrix[y][x]
