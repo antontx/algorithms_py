f3645650800a66b19cdb0a4c6cd7e26c7f981af7
ntx <87873355+antontx@users.noreply.github.com>
2023-03-02 10:14:00 +0100

    Update euclidianAlgorithm.py
    added extended version, removed unnecessary prints

--- euclidianAlgorithm.py
+++ euclidianAlgorithm.py
@@ -11,12 +11,41 @@ def gcd(a: int, b:int) -> int:
             return r
         r = a % b
 
-
-        print(a,q,b,r)
-
         a = b
         b = r
 
+
+def extgcd(a: int, b:int) -> int:
+    r = b
+    steps = []
+    
+    #euclidean 
+    while True:
+        q = a//b
+        if a%b == 0:
+            break
+        r = a % b
+
+
+        steps.append([a,q,b,r])
+
+        a = b
+        b = r
+    
+    #extension
+    x = 1
+    a = steps[-1][0]
+    y = steps[-1][1]*-1
+    b = steps[-1][2]
+    
+    for i in range(len(steps)-1):
+        b = a
+        a = steps[-2-i][0]
+        _y = y
+        y = x + (steps[-2-i][1]*-1)*y
+        x = _y
+        
+    return(r,x,y)
 
 
 
