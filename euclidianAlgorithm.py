45eb99be0a8b0bde72f1161951cfd83909bc35e7
antontx <tarisjan@gmail.com>
2023-03-06 17:20:22 +0100

    implementation of the euclidian algorithm that is able to find the greatest common dividers of two integers

--- euclidianAlgorithm.py
+++ euclidianAlgorithm.py
@@ -1,5 +1,5 @@
 def main():
-    print(gcd(192,35))
+    print(str_extgcd(37,60))
 
 
 def gcd(a: int, b:int) -> int:
@@ -15,7 +15,7 @@ def gcd(a: int, b:int) -> int:
         b = r
 
 
-def extgcd(a: int, b:int) -> int:
+def extgcd(a: int, b:int) -> tuple[int, int, int]:
     r = b
     steps = []
     
@@ -46,6 +46,47 @@ def extgcd(a: int, b:int) -> int:
         x = _y
         
     return(r,x,y)
+def str_extgcd(a: int, b:int) -> str:
+    r = b
+    steps = []
+    out = []
+
+    #euclidean
+    while True:
+        q = a//b
+        if a%b == 0:
+            break
+        r = a % b
+
+
+        steps.append([a,q,b,r])
+        out.append(f"{a}={q}*{b}+{r}")
+
+        a = b
+        b = r
+
+    out.append(f"{a}={q}*{b}+0")
+    out.append("")
+
+    #extension
+    x = 1
+    a = steps[-1][0]
+    y = steps[-1][1]*-1v
+
+    out.append(f"{r}={x}*{a}+{y}*{b}")
+
+    for i in range(len(steps)-1):
+        b = a
+        a = steps[-2-i][0]
+        _y = y
+        y = x + (steps[-2-i][1]*-1)*y
+        x = _y
+        out.append(f"{r}={x}*{a}+{y}*{b}")
+
+
+    out = "\n".join(out).replace("+-","-").replace("*","\u2022")
+
+    return out
 
 
 
