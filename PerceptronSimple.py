f361f77db57c7095f37464cc52ea39e7485575bb
ntx <87873355+antontx@users.noreply.github.com>
2023-04-07 06:56:27 +0000

    updatecperceptron

--- PerceptronSimple.py
+++ PerceptronSimple.py
@@ -1,24 +1,78 @@
 import numpy as np
+import os
+import matplotlib.pyplot as plt
 
 def generate_data(n):
-    features = []
-    labels = []
+    """
+    This function generates a linearly separable dataset with 2 features.
 
-    for _ in range(n):
-        temp = np.random.randint(0, 41)
+    Args:
+    n: size of the dataset
 
-        humidity = np.random.randint(0, 101)
+    Returns:
+    A tuple containing two lists:
+        - features: list of n-dimensional vectors representing features
+        - labels: list of binary labels (0 or 1) representing classes
+    """
 
-        feature_vector = np.array([temp, humidity])
-        features.append(feature_vector)
+    # Generate random linearly separable data
+    mean1 = np.random.randint(low=-10, high=0, size=(2,))
+    mean2 = np.random.randint(low=0, high=10, size=(2,))
+    cov = np.identity(2) * 4
+    x1 = np.random.multivariate_normal(mean1, cov, n // 2)
+    x2 = np.random.multivariate_normal(mean2, cov, n // 2)
 
-        # Label the data point as 1 (good) if temperature is above 25 degrees Celsius and humidity is below 50%, otherwise 0 (bad)
-        if temp > 25 and humidity < 50:
-            labels.append(1)
-        else:
-            labels.append(0)
+    # Concatenate the data and labels
+    features = np.concatenate([x1, x2], axis=0)
+    labels = np.concatenate([np.zeros(n // 2), np.ones(n // 2)])
 
-    return features, labels
+    # Shuffle the data
+    idx = np.random.permutation(n)
+    features = features[idx]
+    labels = labels[idx]
+
+    return features.tolist(), labels.tolist()
+
+
+def plot_data(features, labels,filename=None, weight_vector=None,format='png'):
+    """
+    This function plots the generated data with the labels and saves it as an image.
+
+    Args:
+    features: list of n-dimensional vectors representing features
+    labels: list of binary labels (0 or 1) representing classes
+    filename: name of the file to save the image
+    format: format of the image (default: png)
+
+    Returns:
+    None
+    """
+
+    plt.clf()
+
+    # Convert lists to arrays
+    features = np.array(features)
+    labels = np.array(labels)
+
+    # Plot the data
+    plt.scatter(features[:, 0], features[:, 1], c=labels)
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.title('Linearly Separable Dataset')
+
+    if weight_vector is not None:
+        # Define the x and y coordinates for the decision boundary
+        x = np.linspace(np.min(features[:, 0]), np.max(features[:, 0]), 100)
+        y = (-weight_vector[0] * x) / weight_vector[1]
+
+        # Plot the decision boundary
+        plt.plot(x, y, color='green', label='Decision boundary')
+
+    # Save the plot as an image
+    if filename is not None:
+        plt.savefig(filename, format=format)
+
+    plt.show()
 
 
 def step(x):
@@ -28,7 +82,7 @@ def step(x):
 
 
 class Perceptron:
-    def __init__(self, input_size, learning_rate = 0.1) -> None:
+    def __init__(self, input_size, learning_rate = 0.01) -> None:
         self.learning_rate = learning_rate
         self.weights = np.random.randn(input_size,1)
         self.bias = np.random.randn()
@@ -37,10 +91,10 @@ class Perceptron:
         z = np.dot(X,self.weights) + self.bias
         return step(z)
  
-    def train(self, features, labels, epochs = 100):
+    def train(self, training_features, training_labels, epochs = 100):
         for epoch in range(epochs):
             total_error = 0
-            for X, label in zip(features,labels):
+            for X, label in zip(training_features,training_labels):
                 error = label - self.classify(X)
                 #print(f"{error=}")
 
@@ -52,21 +106,36 @@ class Perceptron:
                 total_error += abs(error)
 
             if epoch % 10 == 0:
-                print(f"Epoch {epoch}: Error = {total_error}")    
-            
+                print(f"Epoch {epoch}: Error = {total_error}")
+
+            elif total_error == 0:
+                print(f"Epoch {epoch}: Error = {total_error}")
+                break
+        print(f"Epoch {epoch}: Error = {total_error}")
+        
+    
+    def eval(self ,features,labels):
+        correct = 0
+        for x,y in zip(features,labels):
+            if self.classify(x) == y:
+                correct += 1
+        
+        return f"{(correct/len(features))*100}%"
     
     def out(self):
         print(f"weights\n{self.weights}\n\nbias\n{self.bias}")
 
-np.random.seed(22)
-
-training_features, training_labels = generate_data(10000)
-
+np.random.seed(890)
 
 p1 = Perceptron(2)
+
+training_features, training_labels = generate_data(1000)
+
+plot_data(training_features,training_labels,"pretrain.png",p1.weights)
+
+
+print(p1.eval(training_features,training_labels))
 p1.train(training_features,training_labels,1000)
+print(p1.eval(training_features,training_labels))
+plot_data(training_features,training_labels,"postrain.png",p1.weights)
 
-#print(training_features[3])
-
-index = 9
-print(p1.classify(training_features[index]),training_labels[index])
